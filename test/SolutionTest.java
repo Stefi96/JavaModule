4b4c7d0919e90598be8b7c0e2e8afb3ee73097e6
stefi96 <stefanhrmail@gmail.com>
2022-09-23 12:43:39 +0200

    refactor test

--- test/SolutionTest.java
+++ test/SolutionTest.java
@@ -1,22 +1,21 @@
-package main;
-
+import main.SimpleCalc;
+import main.Solution;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
     @Test
     void count_letter() {
         System.out.println("Poceo test");
-        assertEquals(1,Solution.count_letter("stefan","a"));
+        Assertions.assertEquals(1, Solution.count_letter("stefan","a"));
     }
 
     @Test
     void testCalc() {
         System.out.println("Poceo test");
-        assertEquals(2,SimpleCalc.simpleCalcToTest(1,1));
+        Assertions.assertEquals(2, SimpleCalc.simpleCalcToTest(1,1));
     }
 
     @AfterEach
